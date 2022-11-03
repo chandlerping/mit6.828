c13504ff4d29e6d8257555735f03122bd70be2d6
chandlerping <jingping_wan@163.com>
2022-11-03 23:21:00 +0800

    modify SYS_env_destroy

--- kern/syscall.c
+++ kern/syscall.c
@@ -85,7 +85,7 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
 		return sys_getenvid();
 		break;
 	case SYS_env_destroy:
-		return sys_env_destroy(curenv->env_id);
+		return sys_env_destroy((envid_t)a1);
 		break;
 
 	default:
