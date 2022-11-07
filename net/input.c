d883e81aaa7a6e9741cb2a279be79ad4cc1ecdb5
chandlerping <jingping_wan@163.com>
2022-11-07 12:20:57 +0800

    lab 6

--- net/input.c
+++ net/input.c
@@ -4,40 +4,40 @@
 extern union Nsipc nsipcbuf;
 
 
- void
+void
 sleep(int msec)
 {
-       unsigned now = sys_time_msec();
-       unsigned end = now + msec;
+  unsigned now = sys_time_msec();
+  unsigned end = now + msec;
 
-       if ((int)now < 0 && (int)now > -MAXERROR)
-               panic("sys_time_msec: %e", (int)now);
+  if ((int)now < 0 && (int)now > -MAXERROR)
+    panic("sys_time_msec: %e", (int)now);
 
-       while (sys_time_msec() < end)
-               sys_yield();
+  while (sys_time_msec() < end)
+    sys_yield();
 }
 
 void
 input(envid_t ns_envid)
 {
-	binaryname = "ns_input";
+  binaryname = "ns_input";
 
-	// LAB 6: Your code here:
-	// 	- read a packet from the device driver
-	//	- send it to the network server
-	// Hint: When you IPC a page to the network server, it will be
-	// reading from it for a while, so don't immediately receive
-	// another packet in to the same physical page.
+  // LAB 6: Your code here:
+  // 	- read a packet from the device driver
+  //	- send it to the network server
+  // Hint: When you IPC a page to the network server, it will be
+  // reading from it for a while, so don't immediately receive
+  // another packet in to the same physical page.
 
   size_t len;
   char buf[RX_PKT_SIZE];
   while (1) {
-		if (sys_packet_try_recv(buf, &len) < 0) {
-			continue;
+    if (sys_packet_try_recv(buf, &len) < 0) {
+      continue;
     }
-		memcpy(nsipcbuf.pkt.jp_data, buf, len);
-		nsipcbuf.pkt.jp_len = len;
-    ipc_send(ns_envid, NSREQ_INPUT, &nsipcbuf, PTE_P|PTE_U|PTE_W);
-		sleep(50);
+    memcpy(nsipcbuf.pkt.jp_data, buf, len);
+    nsipcbuf.pkt.jp_len = len;
+    ipc_send(ns_envid, NSREQ_INPUT, &nsipcbuf, PTE_P | PTE_U | PTE_W);
+    sleep(50);
   }
 }
